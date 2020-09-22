512acb5fac55139f0d4bf3e66d93337ebd9d4cac
oliverochman <oliver.ochman@hotmail.com>
2020-09-22 10:48:15 +0200

    add products index route and products controller with index action

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,9 @@ Rails.application.routes.draw do
     namespace :v0 do
       resources :pings, only: [:index], constraints: { format: 'json' }
     end
+
+    namespace :v1 do
+      resources :products, only: [:index], constraints: { format: 'json' }
+    end
   end
 end
