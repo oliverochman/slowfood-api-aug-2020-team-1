a6cc01af40190933b7c466b03d6b566c3f3c6db9
oliverochman <oliver.ochman@hotmail.com>
2020-09-22 10:48:56 +0200

    add response_json test helper

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -13,6 +13,7 @@ ActiveRecord::Migration.maintain_test_schema!
 Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
 
 RSpec.configure do |config|
+  config.include ResponseJSON
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
   config.use_transactional_fixtures = true
   config.infer_spec_type_from_file_location!
