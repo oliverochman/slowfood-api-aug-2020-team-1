74bff20ed182c100762546dc4525d45fe01714b5
oliverochman <oliver.ochman@hotmail.com>
2020-09-23 10:14:31 +0200

    implement logic for order update action

--- app/controllers/api/v1/orders_controller.rb
+++ app/controllers/api/v1/orders_controller.rb
@@ -3,12 +3,19 @@ class Api::V1::OrdersController < ApplicationController
 
   def create
     order = Order.create(user: current_user)
-    order.order_items.create(product_id: params["product"])
+    product = Product.find(params[:product])
 
-    render json: { id: order.id, message: "Item has been added to your order"}
+    order.order_items.create(product: product)
+
+    render json: { order_id: order.id, message: "Item has been added to your order"}
   end
 
   def update
-    
+    order = Order.find(params[:id])
+    product = Product.find(params[:product])
+
+    order.order_items.create(product: product)
+
+    render json: { order_id: order.id, message: "Item has been added to your order"}
   end
 end
