e2a0bae464408e244637e9e52e7204fe0caa43ca
oliverochman <oliver.ochman@hotmail.com>
2020-09-28 15:38:42 +0200

    send request to brottskartan in index action and send back data

--- app/controllers/api/v2/crime_reports_controller.rb
+++ app/controllers/api/v2/crime_reports_controller.rb
@@ -1,5 +1,8 @@
 class Api::V2::CrimeReportsController < ApplicationController
   def index
-    binding.pry
+    response = RestClient.get("https://brottsplatskartan.se/api/events/?location=#{params[:q]}")
+    parsed_response = JSON.parse(response.body)
+
+    render json: { crime_reports: parsed_response["data"] }
   end
 end
--- spec/requests/api/v2/crime_reports/index_spec.rb
+++ spec/requests/api/v2/crime_reports/index_spec.rb
@@ -10,11 +10,11 @@ RSpec.describe 'GET /api/v2/crime_reports', type: :request do
     expect(response.status).to eq 200
   end
 
-  it 'is expected to return 20 crime reports' do
-    expect(response_json["crime_reports"].count).to eq 20
+  it 'is expected to return 10 crime reports' do
+    expect(response_json["crime_reports"].count).to eq 10
   end
 
   it 'is expected to return specific crime report' do
-    expect(response_json["crime_reports"].second["description"]).to eq "Narkotika"
+    expect(response_json["crime_reports"].second["title_type"]).to eq "Narkotikabrott"
   end
 end
