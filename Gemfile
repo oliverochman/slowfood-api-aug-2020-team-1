2e2999036b5ece6ab5d9850f0cbc939ea12c5d78
oliverochman <oliver.ochman@hotmail.com>
2020-09-28 15:59:41 +0200

    add sad path and webmock

--- Gemfile
+++ Gemfile
@@ -21,6 +21,7 @@ group :development, :test do
   gem 'rspec-rails'
   gem 'shoulda-matchers'
   gem 'factory_bot_rails'
+  gem 'webmock'
 end
 
 group :development do
--- app/controllers/api/v2/crime_reports_controller.rb
+++ app/controllers/api/v2/crime_reports_controller.rb
@@ -2,7 +2,11 @@ class Api::V2::CrimeReportsController < ApplicationController
   def index
     response = RestClient.get("https://brottsplatskartan.se/api/events/?location=#{params[:q]}")
     parsed_response = JSON.parse(response.body)
-
-    render json: { crime_reports: parsed_response["data"] }
+    
+    if parsed_response["data"].empty?
+      render json: { error_message: "No search results"}, status: 401
+    else 
+      render json: { crime_reports: parsed_response["data"] }
+    end
   end
 end
